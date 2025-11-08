b4d5a52c2a327b04f1c878c301102659f8384d3c
Marcel Malewski <mmalewski9@gmail.com>
2025-11-08 16:02:02 +0100

    Finished help panel

--- ui/src/app/component/help-panel/help-panel.component.ts
+++ ui/src/app/component/help-panel/help-panel.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import {
+    Command,
+    CommandsValues,
+    CommandsWithParam,
+} from '../../spec/common-spec';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
     selector: 'help-panel',
     standalone: true,
     templateUrl: './help-panel.component.html',
-    imports: [],
+    imports: [TitleCasePipe],
 })
-export class HelpPanelComponent {}
+export class HelpPanelComponent {
+    commandsValues = CommandsValues;
+    commandsWithParam = CommandsWithParam;
+
+    // Generate a placeholder dynamically based on the command
+    prepareCommandLabel(command: Command): string {
+        if (this.commandsWithParam.includes(command)) {
+            // Convert command string to friendly placeholder
+            // e.g., "set seconds" → "set seconds {secondsValue}"
+            const words = command.split(' '); // split into words
+            const paramName = words.slice(-1)[0]; // take the last word
+            return `${command} {${paramName}Value}`;
+        }
+        return command;
+    }
+}
